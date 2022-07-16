223ef77c4fb9b85a29b0bbc7e1bef31f142cb1e6
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-16 22:10:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="spinner">
+        Loading
+        <div className="spinner-sector spinner sector-red"></div>
+      </div>
+    </div>
+  );
+}
