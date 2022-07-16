c381bc0ddcd5261a1079eec837c0be5d9855ac09
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-16 22:12:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
+      {/* FIRST SPINNER */}
       <div className="spinner">
         Loading
         <div className="spinner-sector spinner sector-red"></div>
+        <div className="spinner-sector spinner sector-blue"></div>
+        <div className="spinner-sector spinner sector-green"></div>
       </div>
+      {/* SECOND SPINNER */}
+      <div className="spinner-2">Loading</div>
     </div>
   );
 }
